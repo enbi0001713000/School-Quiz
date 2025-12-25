c1de5cee4ae53e00d7e766264229179a993fea7a
enbi0001713000 <f94z54vrd2@privaterelay.appleid.com>
2025-12-25 20:43:51 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,19 @@
 const CACHE_NAME = "school-quiz-v5";
 const ASSETS = ["./", "./index.html", "./bank.js", "./app.js", "./manifest.json"];
 
-
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k))))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k)))))
+      .then(() => self.clients.claim())
   );
 });
 
@@ -21,5 +22,3 @@ self.addEventListener("fetch", (event) => {
     caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
-
-
